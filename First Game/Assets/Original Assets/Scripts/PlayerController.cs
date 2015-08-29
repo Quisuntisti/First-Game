62d5a7cd9c62eb7358c531528b4512ce2b972d2d
genfy <alexmainstone@atheist.com>
2015-08-29 09:05:37 +0100

    Basic Movement
    -Added simple jumping.
    -Added simple movement.
    -Added a simple follow camera.
    -Ground checking added, stops the player from jumping in the air.

--- First Game/Assets/Original Assets/Scripts/PlayerController.cs
+++ First Game/Assets/Original Assets/Scripts/PlayerController.cs
@@ -2,14 +2,40 @@ using UnityEngine;
 using System.Collections;
 
 public class PlayerController : MonoBehaviour {
-	
+
+	public float moveSpeed;
+	public float jumpSpeed;
+
+	private bool grounded;
+	public LayerMask whatIsGround;
+	public Transform groundCheck;
+	public float groundCheckRadius;
+
+	private Rigidbody2D rb;
+
 	// Use this for initialization
 	void Start () {
-	
+		rb = gameObject.GetComponent<Rigidbody2D>();
 	}
 	
 	// Update is called once per frame
 	void Update () {
-	
+
+		if (Input.GetKeyDown (KeyCode.Space) && grounded) {
+			rb.velocity = new Vector2(rb.velocity.x,jumpSpeed);
+		}
+
+		if (Input.GetKey (KeyCode.D)) {
+			rb.velocity = new Vector2(moveSpeed, rb.velocity.y);
+		}
+
+		if (Input.GetKey (KeyCode.A)) {
+			rb.velocity = new Vector2(-moveSpeed, rb.velocity.y);
+		}
+
+	}
+
+	void FixedUpdate(){
+		grounded = Physics2D.OverlapCircle (groundCheck.position, groundCheckRadius, whatIsGround);
 	}
 }
